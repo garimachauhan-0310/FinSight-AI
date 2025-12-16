fd18822098c7c601c872bb1815d66d2b0d3eb277
Garima Chauhan <garimachauhan@Garimas-MacBook-Pro.local>
2025-12-16 22:43:34 +0530

    Added centralized logic and data layer

---
+++ Frontend/my-app/src/lib/mockData.ts
@@ -0,0 +1,17 @@
+export const userProfile = {
+  age: 21,
+  riskScore: 5, // 1–10
+  riskLevel: "Moderate",
+  timeHorizon: 5,
+};
+
+export const portfolioAllocation = {
+  equity: 60,
+  debt: 30,
+  cash: 10,
+};
+
+export const metrics = {
+  expectedReturn: 9.8,
+  volatility: 12.4,
+};
